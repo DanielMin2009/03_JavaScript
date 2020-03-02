ee1e68d5a31ee5d08b11395ebe56b6be0926720b
DanielMin2009 <daniel.minguella.blasco@gmail.com>
2020-03-02 13:19:57 +0100

    rating js finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,115 +1,37 @@
-//var button01 = document.forms["splitNameForm"]["btnSplitName"];
-var star01 = document.getElementById("starItem01");
-var star02 = document.getElementById("starItem02");
-var star03 = document.getElementById("starItem03");
-var star04 = document.getElementById("starItem04");
-var star05 = document.getElementById("starItem05");
+var poopIconsList = document.querySelectorAll(".pinkStar");
 
-var stars = document.getElementById("iconsGrid-5");
-
-star01.addEventListener("onmouseover", activateStar01, true);
-star02.addEventListener("onmouseover", activateStar02, true);
-star03.addEventListener("onmouseover", activateStar03, true);
-star04.addEventListener("onmouseover", activateStar04, true);
-star05.addEventListener("onmouseover", activateStar05, true);
-
-
-
-
-
-
-/*
-var activateStar01 = () => {
-    star01.removeAttribute("class", "hotpink");
-    star01.setAttribute("class", "brownie");
+function setFixedColor(starItemId) {
+    //Agafa l'id, i li extreu el numero de l'icone PoopStar. ID --> starItem"02"
+    let starItem = starItemId.substr(8);
+    console.log(starItem);
+    let i;
+    //document.getElementById("iconsGrid-5").removeAttribute("class");
+    //Afegeix la class chocoStar al dau clicat i a tots els anteriors. 
+    for (i = 1; i <= starItem; i++) {
+        document.getElementById("starItem" + i).classList.add("chocoStar");
+    }
+    //Treu la class painted als daus posteriors al dau clicat. Si és major que l'identificatiu o id fins que sigui igual al total d'icones que hi hagi a la llista
+    for (i > starItem; i <= poopIconsList.length; i++) {
+        document.getElementById("starItem" + i).classList.remove("chocoStar");
+    }
+    //document.getElementById("iconsGrid-5").setAttribute("class", "stars");
 }
 
-function activateStar01() {
-    star01.removeAttribute("class", "hotpink");
-    star01.setAttribute("class", "brownie");
-}
-
-
-var activateStar02 = (star02) => {
-    star02.removeAttribute("class", "hotpink");
-    star02.setAttribute("class", "brownie");
-    return star02;
-}
-*/
-
-
-
-
-var mapName = (item1, item2) => {
-
-
-
-    let name = document.getElementById("txtName03").value;
-    let arrName = new Array();
-    arrName = name.split("");
-    let character;
-
-    let isNumber = /\d/g;
-    let isLetter = /[a-z]/gi;
+document.addEventListener("click", function (event) {
+    //Es captura el lloc del document on s'ha clicat sigui quin sigui.
+    var clearData = event.target;
+    var poopBox = document.getElementById("iconsGrid-5");
+    let i;
+    //Si el lloc de la pantalla on s'ha clicat és dins del div amb l'id d'icons, torna false. 
+    //poopBox forma part de clearData?
+    if (!poopBox.contains(clearData)) {
+        for (i = 1; i <= poopIconsList.length; i++) {
+            document.getElementById("starItem" + i).classList.remove("chocoStar");
+        }
+    }
+});
 
 
 
 
-    return `Letters ${item1}, Times ${item2}!`;
-}
 
-if (arrName[character].match(isLetter)) {
-
-}
-
-
-
-/*
-var hello3 = (item1, item2) => {
-    return `Hello ${item1}, good bye ${item2}!`;
-}
-console.log(hello3("World", "Moon"));
-*/
-
-
-
-/*
-var hello1 = function () {
-    return "Hello World!";
-}
-
-console.log(hello1());
-
-
-//===================(Arrow Function notation)
-
-//multiple line block
-var hello2 = () => {
-    return "Hello World!";
-}
-
-console.log(hello2());
-
-
-//Esto se puede hacer en una sola linea de código (Sign line Block)
-var hello3 = () => "Hello World!";
-console.log(hello3());
-
-//===================
-
-/*
-
-${} sustituye a + ítem +
-function makeShoppingList(item1='milk', item2='bread', item3='eggs'){
-  console.log(`Remember to buy ${item1}`);
-  console.log(`Remember to buy ${item2}`);
-  console.log(`Remember to buy ${item3}`);
-}
-makeShoppingList('orange','hello');
-
-
-var hello3 = (item1, item2) => {
-    return `Hello ${item1}, good bye ${item2}!`;
-}
-console.log(hello3("World", "Moon"));
-*/
